2ea96f1dc78adf60f30a29a0a4957865371a536a
ThomasBoxall <38958967+ThomasBoxall@users.noreply.github.com>
2022-12-06 12:09:58 +0000

    start python item 3 challenge feature

--- 01-FIRST-YEAR/M30299 (Programming)/coursework01/challengeFeature.py
+++ 01-FIRST-YEAR/M30299 (Programming)/coursework01/challengeFeature.py
@@ -48,7 +48,7 @@ def drawCirclefromTL(win, tlPoint, radius, colour):
     return circ
 
 # DRAW SINGLE PATCH FUNCTIONS
-def drawCirclePatch(win, colour, tl):
+def drawCirclePatch(win, colour, tl, patchArray):
     """Draws single square then calls drawFourSmallCircles() to draw alternating colour circles in that rectangle"""
     patchSize = 100
     increment = 20
@@ -58,15 +58,15 @@ def drawCirclePatch(win, colour, tl):
             currentTopLeft = Point(x, y)
             if altFlag:
                 # use the user inputted colour
-                drawRectangle(win, currentTopLeft, getBrPoint(currentTopLeft, increment, increment), colour)
+                patchArray[int(tl.getY()/100)][int(tl.getX()/100)].append(drawRectangle(win, currentTopLeft, getBrPoint(currentTopLeft, increment, increment), colour))
             else:
                 # use colour as white
-                drawRectangle(win, currentTopLeft, getBrPoint(currentTopLeft, increment, increment), "white")
+                patchArray[int(tl.getY()/100)][int(tl.getX()/100)].append(drawRectangle(win, currentTopLeft, getBrPoint(currentTopLeft, increment, increment), "white"))
             # now draw the four smaller circles inside the square
-            drawFourSmallCircles(win, colour, currentTopLeft, altFlag)
+            drawFourSmallCircles(win, colour, currentTopLeft, altFlag, patchArray, tl)
             altFlag = not altFlag # switch the flag so we use the other colours on the next iteration
 
-def drawFourSmallCircles(win, colour, tl, altFlag):
+def drawFourSmallCircles(win, colour, tl, altFlag, patchArray, paTL):
     """Draws four little circles in a 20x20 space, based from TopLeft (tl) point of specified colour or white if altFlag=True"""
     # all circles have radius 5
     for y in range(int(tl.getY()), int(tl.getY()+20), 10):
@@ -74,13 +74,13 @@ def drawFourSmallCircles(win, colour, tl, altFlag):
             currentTopLeft = Point(x, y)
             if altFlag:
                 # use colour as white
-                drawCirclefromTL(win, currentTopLeft, 5, "white")
+                patchArray[int(paTL.getY()/100)][int(paTL.getX()/100)].append(drawCirclefromTL(win, currentTopLeft, 5, "white"))
             else:
                 # use colour as use inputted colour
-                drawCirclefromTL(win, currentTopLeft, 5, colour)
+                patchArray[int(paTL.getY()/100)][int(paTL.getX()/100)].append(drawCirclefromTL(win, currentTopLeft, 5, colour))
 
 
-def drawLinePatch(win, colour, tl):
+def drawLinePatch(win, colour, tl, patchArray):
     """Draw a single 'line' patch with TopLeft point tl"""
     # we add 100 as this moves to the other side of the patch
     # we add 10 as this is the gap between the lines
@@ -89,29 +89,31 @@ def drawLinePatch(win, colour, tl):
         # define start and end points for line one
         startOne = Point(offset + tl.getX(), tl.getY())
         endOne = Point(tl.getX()+100, offset + tl.getY() + 10)
-        drawLine(win, startOne, endOne, colour) # draw line one
+        patchArray[int(tl.getY()/100)][int(tl.getX()/100)].append(drawLine(win, startOne, endOne, colour)) # draw line one
         # define start and end points of line two
         startTwo = Point(tl.getX(), tl.getY()+offset)
         endTwo = Point(offset + tl.getX() + 10, tl.getY()+100)
-        drawLine(win, startTwo, endTwo, colour) # draw line two
+        patchArray[int(tl.getY()/100)][int(tl.getX()/100)].append(drawLine(win, startTwo, endTwo, colour)) # draw line two
 
-def drawPlainPatch(win, colour, tl):
+def drawPlainPatch(win, colour, tl, patchArray):
     """Draw a plain patch of specified colour"""
-    drawRectangle(win, tl, getBrPoint(tl, 100, 100), colour)
+    patchArray[int(tl.getY()/100)][int(tl.getX()/100)].append(drawRectangle(win, tl, getBrPoint(tl, 100, 100), colour))
 
 
-def populateWindow(win, colours, dimension):
+def populateWindow(win, colours, dimension, patchArray):
     for y in range(0,dimension,100):
+        patchArray.append([])
         for x in range(0,dimension,100):
+            patchArray[int(y/100)].append([])
             topLeft = Point(x,y)
             if x >= 100 and y >= 100 and x<dimension-100 and y<dimension-100:
                 #we actually need to draw a patch now
                 if x == y or x+y == dimension - 100:
-                    drawLinePatch(win, getColour(colours, topLeft, dimension), topLeft)
+                    drawLinePatch(win, getColour(colours, topLeft, dimension), topLeft, patchArray)
                 else:
-                    drawCirclePatch(win, getColour(colours, topLeft, dimension), topLeft)
+                    drawCirclePatch(win, getColour(colours, topLeft, dimension), topLeft, patchArray)
             else:
-                drawPlainPatch(win, getColour(colours, topLeft, dimension), topLeft)
+                drawPlainPatch(win, getColour(colours, topLeft, dimension), topLeft, patchArray)
 
 def getColour(colours, tl, dimension):
     """takes colours (list), top left (point) and dimension (int) then returns colour (str) for correct location on grid"""
@@ -197,13 +199,32 @@ def winInit(size):
     win.setBackground("white")
     return win
 
+def getPatch(clickPos):
+    # will take input like Point(233,434) and need to return Point(200,400)
+    x = clickPos.getX()
+    y = clickPos.getY()
+    xCoord = x // 100
+    yCoord = y // 100
+    return Point(xCoord * 100, yCoord * 100), xCoord, yCoord
+
 def main():
     colours, size = getUserInput()
     # at this point the user inputs are valid and can be manipulated
     size = size * 100 # increase size to 100 times input size so that its correct scale to pass into the GraphWin constructor and populateWindow() functions
     win = winInit(size)
-    populateWindow(win, colours, size)
-    win.getMouse() # ask about this line and if it should exist or not?
-
+    patchArray = []
+    populateWindow(win, colours, size, patchArray)
+    # while True:
+    #     print(getPatch(win.getMouse()))
+    print(patchArray)
+    
+    pos = win.getMouse() # ask about this line and if it should exist or not?
+    print(patchArray[int(pos.getY()/100)][int(pos.getX()/100)])
+    for each in patchArray[int(pos.getY()/100)][int(pos.getX()/100)]:
+        # patchArray.remove(each)
+        each.undraw()
+    patchArray[int(pos.getY()/100)][int(pos.getX()/100)].clear()
+    print("x", patchArray[int(pos.getY()/100)][int(pos.getX()/100)])
+    win.getMouse()
 
 main()
