54bbad4c1c1f07bfa326fd21de810801e2d18677
ThomasBoxall <38958967+ThomasBoxall@users.noreply.github.com>
2024-02-03 12:28:31 +0000

    actually commit the file

--- 01-L5/M21274-DMAFP/week2.hs
+++ 01-L5/M21274-DMAFP/week2.hs
@@ -70,5 +70,86 @@ daysInMonth m y
     | m == 2 = daysInFeb y
     | m == 4 || m == 6 || m == 9 || m == 11 = 30
     | otherwise = 31 -- we can assume all inputs will be valid so can just do this
+    -- to handle invalid cases, we'd want to use the first guard fro validDate to return 31, then have our otherwise guard as returning 0 or a value which clearly indicates an invalid value has been entered.
     where
         daysInFeb y = if (mod y 4 == 0) then 29 else 28
+
+-- written exercises
+
+-- q1
+{-
+sumThree 3 5 7    
+~> 3 + 5 + 7           definition of sumThree
+~> 3 + 12              arithmetic
+~> 15                  arithmetic
+
+sumThree 8 (1 + 3) 2
+~> 8 + (1 + 3) + 2     definition of sumThree
+~> 8 + 4 + 2           arithmetic
+~> 12 + 2              arithmetic
+~> 14                  arithmetic
+-}
+
+-- q2
+{-
+threeDifferent 1 4 2
+~> different 1 4 && different 1 2 && different 4 2 && different 1 2     definition of threeDifferent
+~> 1 /= 4                                                               definition of first call of different
+~> True                                                                 def of /=
+~> True && different 1 2 && different 4 2 && different 1 2              updated definition of threeDifferent
+~> 1 /= 2                                                               definition of second call of different
+~> True                                                                 def of /=
+~> True && True && different 4 2 && different 1 2                       updated definition of threeDifferent
+~> 4 /= 2                                                               definition of third call of different
+~> True                                                                 def of /=
+~> True && True && True && different 1 2                                updated definition of threeDifferent
+~> 1 /= 2                                                               definition of fourth (final) call of different
+~> True                                                                 def of /=
+~> True && True && True && True                                         updated definition of threeDifferent
+~> True                                                                 result
+
+threeDifferent 1 7 7
+~> different 1 7 && different 1 7 && different 7 7 && different 1 7     definition of threeDifferent
+~> 1 /= 7                                                               definition of first call of different
+~> True                                                                 def of /=
+~> True && different 1 7 && different 7 7 && different 1 7              updated definition of threeDifferent
+~> 1 /= 7                                                               definition of second call of different
+~> True                                                                 def of /=
+~> True && True && different 7 7 && different 1 7                       updated definition of threeDifferent
+~> 7 /= 7                                                               definition of third call of different
+~> False                                                                def of /=
+~> True && True && False && different 1 7                               updated definition of threeDifferent
+~> 1 /= 7                                                               definition of fourth (final) call of different
+~> True                                                                 def of /=
+~> True && True && False && True                                        updated definition of threeDifferent
+~> False                                                                result  
+-}
+
+-- q3
+{-
+howManyEqual 3 5 2
+??  3 == 5 && 5 == 2       first guard
+??  ~> False && 5 == 2     def of ==
+??  ~> False && False      def of ==
+??  ~> False               def of &&
+??  3 == 5                 second guard
+??  ~> False               def of ==
+??  3 == 2                 third guard
+??  ~> False               def of ==
+??  5 == 2                 fourth guard
+??  ~> False               def of ==
+??  otherwise              fifth guard
+??  ~> True                def of otherwise
+~> 0
+
+howManyEqual 5 2 5
+??  5 == 2 && 2 == 5       first guard
+??  ~> False && 2 == 5     def of ==
+??  ~> False && False      def of ==
+??  ~> False               def of &&
+??  5 == 2                 second guard
+??  ~> False               def of ==
+??  5 == 5                 third guard
+??  ~> True                def of ==
+~> 2
+-}
